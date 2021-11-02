0e61db2d3c628bbaf3ab954663e2ea333f4a6511
satanya <admin@sozr.cn>
2021-11-02 21:56:23 +0800

    implement the slice

--- src/telegram.rs
+++ src/telegram.rs
@@ -1,46 +1,49 @@
-use core::{convert::From, fmt::Debug, panic};
+use core::{
+    fmt::Debug,
+    ops::{AddAssign, Shl},
+    panic,
+};
 
 type RawSegment = u32;
 const SEGMENT_LEN: u8 = (1024 / RawSegment::BITS) as u8;
+pub type Telegram = [RawSegment; SEGMENT_LEN as usize];
 
-#[derive(Debug)]
-pub struct Telegram {
-    data: [RawSegment; SEGMENT_LEN as usize],
-    len: u16,
+trait ValueTrait<T> {
+    fn value_of(&self, from: u8, to: u8) -> T;
 }
 
-fn range(from: u8, to: u8) -> (u8, u8) {
-    // 最大支援到 128 位，因此to不得大於 128
-    if from > to || to > 128 {
-        panic!("range overflow")
-    }
-
-    (from, to)
-}
-
-impl Telegram {
-    fn value_of<T>(&self, from: u8, to: u8) -> T
-    where
-        T: TryFrom<RawSegment>,
-        <T as TryFrom<RawSegment>>::Error: Debug,
-    {
-        let (from, to) = range(from, to);
-        let (first_pos, last_pos) = (from / SEGMENT_LEN, to / SEGMENT_LEN);
-        for i in first_pos..=last_pos {
-            let segment = self.data[i as usize];
-            // 使用 mask 純化所求區間
-            let purified = segment & segment_mask(from, to);
-            //根據所劃定的寬度決定類型
-            let expanded: T = purified.try_into().unwrap();
+impl<T> ValueTrait<T> for Telegram
+where
+    T: TryFrom<u8> + TryFrom<RawSegment> + AddAssign + Default + Copy + Shl<Output = T>,
+    <T as TryFrom<RawSegment>>::Error: Debug,
+    <T as TryFrom<u8>>::Error: Debug,
+{
+    fn value_of(&self, from: u8, to: u8) -> T {
+        // the width support max to 128
+        if from > to || to - from > 128 {
+            panic!("range overflow")
         }
-        todo!()
-    }
-}
+        let (first_segment_pos, last_segment_pos) = (from / SEGMENT_LEN, (to - 1) / SEGMENT_LEN);
+        let (relative_start, relative_end) = (from % SEGMENT_LEN, (to - 1) % SEGMENT_LEN);
+        // println!("rs: {}, re: {}", relative_start, relative_end);
+        let k = last_segment_pos - first_segment_pos + 1;
+        // println!("k: {}", k);
+        let mut sum = T::default();
+        let mut offset = 0;
 
-/// generate the mask of a u32
-fn segment_mask(from: u8, to: u8) -> RawSegment {
-    if from > to || to - from > SEGMENT_LEN {
-        panic!("range overflow");
+        for i in 0..k {
+            let mut segment = self[(last_segment_pos - i) as usize];
+            if i == k-1 {
+                segment = (segment << relative_start) >> relative_start;
+            }
+            if i == 0 {
+                segment >>= SEGMENT_LEN - relative_end - 1;
+            }
+            //根據所劃定的寬度決定類型
+            let expanded: T = segment.try_into().unwrap();
+            sum += expanded << offset.try_into().unwrap();
+            offset += if i == 0 { relative_end + 1 } else { SEGMENT_LEN };
+        }
+        sum
     }
-    ((1 << (SEGMENT_LEN - from)) - 1) - ((1 << (SEGMENT_LEN - to)) - 1)
 }
